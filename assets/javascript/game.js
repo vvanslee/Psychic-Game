8dfad801ec9c9ff355fe06e1248f15c7b13cf871
vvanslee <vvanslee@gmail.com>
2017-05-21 20:45:59 -0500

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+
+// generate random letter from keyboard
+
+	// var randLetter = chars.substr( Math.floor(Math.random() * 26), 1);
+
+	// function random_character() {
+    // 		var chars = "abcdefghijklmnopqurstuvwxyz";
+
+
+// listen for onkeyup event
+	// document.getElementById("fname").onkeyup = function() {myFunction()};
+
+	// 	function myFunction() {
+	//     	var x = document.getElementById("fname");
+	// }
+
+	// document.onkeyup = function(event) {
+	//		
+	//	}
+
+
+// store user letter
+// compare user input with generated key
+// if correct add 1 to wins
+// else add 1 to losses
+// subract 1 from guesses left
+// add one to guesses so far
